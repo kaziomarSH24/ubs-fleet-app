408e420ac6208c95cdbd9fd5af7d92aba7a0b5e8
Kazi Omar Faruk <kaziomar144@gmail.com>
2026-08-10 12:38:00 +0600

    chore: remove scratch scripts

--- scratch/clear_queue.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:io';
-import 'package:hive/hive.dart';
-import 'package:path_provider/path_provider.dart';
-
-void main() async {
-  Hive.init('./.dart_tool/hive'); // Wait, the path for Flutter hive is different
-}
